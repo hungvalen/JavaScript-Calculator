9b3334eb20f4b4ed1ca83520418916476c7e287e
hungpham <thanhhungpham.902@gmail.com>
2020-09-22 22:15:28 +0700

    make it better

---
+++ 1.js
@@ -0,0 +1,4 @@
+const calculator=document.querySelector('.calulator')
+// Phương thức querySelector() trả về phần tử đầu tiên là phần tử con của phần tử mà nó được gọi ra khớp với nhóm bộ chọn được chỉ định.
+const keys=calculator.querySelector('.calculator_keys')
+keys.addEventListener()
